e19693730124735f5923eba9ee8c504b7626741a
rezwanh001 <rezwanh001@gmail.com>
2025-03-22 04:18:04 -0400

    run shell script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,17 @@
-python main.py --train True --train_gender both --test_gender both --epochs 120 --batch_size 16 --learning_rate 1e-4 --model DepMamba --dataset dvlog-dataset
+python mainkfold.py --train True --num_folds 10 --epochs 125 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion lt --dataset dvlog-dataset
 
-python main.py --train True --train_gender both --test_gender both --epochs 120 --batch_size 16 --learning_rate 1e-4 --model DepMamba --dataset lmvd-dataset
+python mainkfold.py --train True --num_folds 10 --epochs 125 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion it --dataset dvlog-dataset
 
-python main.py --train True --train_gender both --test_gender both --epochs 200 --batch_size 16 --learning_rate 1e-4 --model MultiModalDepDet --dataset dvlog-dataset
-
-python main.py --train True --train_gender both --test_gender both --epochs 200 --batch_size 16 --learning_rate 1e-4 --model MultiModalDepDet --dataset lmvd-dataset
+python mainkfold.py --train True --num_folds 10 --epochs 125 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion ia --dataset dvlog-dataset
 
 
-python main.py --train True --train_gender both --test_gender both --num_folds 10 --epochs 120 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --dataset dvlog-dataset
+python mainkfold.py --train True --num_folds 10 --epochs 125 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion lt --dataset lmvd-dataset
 
-python main.py --train True --train_gender both --test_gender both --num_folds 10 --epochs 120 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --dataset lmvd-dataset
+python mainkfold.py --train True --num_folds 10 --epochs 125 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion it --dataset lmvd-dataset
+
+python mainkfold.py --train True --num_folds 10 --epochs 125 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion ia --dataset lmvd-dataset
+
+
+python main.py --train True --epochs 225 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion lt --dataset dvlog-dataset
+
+python main.py --train True --epochs 225 --batch_size 16 --learning_rate 1e-5 --model MultiModalDepDet --fusion lt --dataset lmvd-dataset
